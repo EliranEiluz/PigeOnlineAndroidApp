c061f2d09562961fce713a9be2b6316ce5273cfa
EliranEiluz <eliranfr1@gmail.com>
2022-06-06 13:52:49 +0300

    adding validity checks to registration and login.

--- app/src/main/java/com/example/pigeonlineandroidapp/MainActivity.java
+++ app/src/main/java/com/example/pigeonlineandroidapp/MainActivity.java
@@ -2,9 +2,14 @@ package com.example.pigeonlineandroidapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActionBar;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.google.firebase.iid.FirebaseInstanceId;
 
@@ -14,11 +19,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Button registerBtn = findViewById(R.id.main_register_btn);
         registerBtn.setOnClickListener(view -> {
             Intent intent = new Intent(this, RegisterActivity.class);
             startActivity(intent);
         });
+
+        Button loginBtn = findViewById(R.id.main_login_btn);
+        loginBtn.setOnClickListener(view -> {
+            EditText userName = findViewById(R.id.main_username);
+            EditText password = findViewById(R.id.main_password);
+            if(!userName.getText().toString().equals("") && !password.getText().toString().equals("")) {
+                // validate with server  username and password, put extra to intent the username and move to chat page.
+                //Intent intent = new Intent(this, contactsActivity.class);
+                //startActivity(intent);
+                return;
+            }
+            else {
+                TextView warningMessage = findViewById(R.id.main_warning_message);
+                warningMessage.setVisibility(View.VISIBLE);
+                ViewGroup.LayoutParams params = warningMessage.getLayoutParams();
+                params.height = ViewGroup.LayoutParams.WRAP_CONTENT;
+                warningMessage.setLayoutParams(params);
+            }
+        });
+
         /*
         FirebaseInstanceId.getInstance().getInstanceId().addOnSuccessListener(
                 MainActivity.this, instanceIdResult -> {
@@ -26,4 +52,5 @@ public class MainActivity extends AppCompatActivity {
                 });
          */
     }
+
 }
--- app/src/main/java/com/example/pigeonlineandroidapp/RegisterActivity.java
+++ app/src/main/java/com/example/pigeonlineandroidapp/RegisterActivity.java
@@ -4,7 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.regex.Pattern;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -16,6 +22,50 @@ public class RegisterActivity extends AppCompatActivity {
         loginBtn.setOnClickListener(view -> {
             Intent intent = new Intent(this, MainActivity.class);
             startActivity(intent);
+        });
+
+        Button registerBtn = findViewById(R.id.register_register_btn);
+        registerBtn.setOnClickListener(view -> {
+            TextView warningMessage = findViewById(R.id.register_warning_message);
+            EditText userName = findViewById(R.id.register_username);
+            ViewGroup.LayoutParams params = warningMessage.getLayoutParams();
+            params.height = ViewGroup.LayoutParams.WRAP_CONTENT;
+            if(userName.getText().toString().equals("")) {
+                warningMessage.setLayoutParams(params);
+                warningMessage.setVisibility(View.VISIBLE);
+                warningMessage.setText("The username field cannot be empty.");
+                return;
+            }
+            EditText password = findViewById(R.id.register_password);
+            if(password.getText().toString().equals("") ||
+                            !Pattern.matches("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{8,20}$",
+                            password.getText().toString())) {
+                warningMessage.setLayoutParams(params);
+                warningMessage.setVisibility(View.VISIBLE);
+                warningMessage.setText("Choose at least 8 characters- one numeric digit, one uppercase and one lowercase letter.");
+                return;
+            }
+            EditText validatePassword = findViewById(R.id.register_validatePass);
+            if(!password.getText().toString().equals(validatePassword.getText().toString())) {
+                warningMessage.setLayoutParams(params);
+                warningMessage.setVisibility(View.VISIBLE);
+                warningMessage.setText("The validate password field must be equal to the password field.");
+                return;
+            }
+            EditText displayName = findViewById(R.id.register_displayName);
+            if(displayName.getText().toString().equals("")) {
+                warningMessage.setLayoutParams(params);
+                warningMessage.setVisibility(View.VISIBLE);
+                warningMessage.setText("The displayName field cannot be empty.");
+                return;
+            }
+
+            // Check Img.
+
+            //Intent intent = new Intent(this, contactsActivity.class);
+            // put extra
+            //startActivity(intent);
+
         });
 
     }

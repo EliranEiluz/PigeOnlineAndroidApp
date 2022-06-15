88a916d53debcf5ed56a2715ab8e2e37ad95d57b
Ben Levi <benlevi7770528@gmail.com>
2022-06-15 17:50:39 +0300

    change theme from settings activity.

--- app/src/main/java/com/example/pigeonlineandroidapp/Activities/SettingsActivity.java
+++ app/src/main/java/com/example/pigeonlineandroidapp/Activities/SettingsActivity.java
@@ -1,6 +1,7 @@
 package com.example.pigeonlineandroidapp.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -18,14 +19,23 @@ public class SettingsActivity extends AppCompatActivity {
 
         EditText defaultServer = findViewById(R.id.settings_server);
         Button applyBtn = findViewById(R.id.settings_apply_btn);
+        Button lightBtn = findViewById(R.id.settings_light_btn);
+        Button nightBtn = findViewById(R.id.settings_night_btn);
+
+        lightBtn.setOnClickListener(view -> {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        });
+        nightBtn.setOnClickListener(view -> {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+        });
 
         applyBtn.setOnClickListener(view -> {
+            Intent intent = new Intent(this, MainActivity.class);
             if(!defaultServer.getText().toString().equals("")) {
-                Intent intent = new Intent(this, MainActivity.class);
                 intent.putExtra("defaultServer", defaultServer.getText().toString());
                 defaultServer.setText("");
-                startActivity(intent);
             }
+            startActivity(intent);
         });
     }
 }
